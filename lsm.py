d6cd0f099de2ab409eb2a8834da8d8dfb3c4a73d
emilholst14 <emilholst14@proton.me>
2025-04-23 10:58:40 +0200

    Update lsm.py

--- lsm.py
+++ lsm.py
@@ -4,6 +4,7 @@ import numpy as np
 from bsm import bsm
 
 def amput_lsm(S0, K, T, r, sigma, N, d, M, kind, av=False, cv=False, seed=42):
+    assert kind == 'call' or kind == 'put', 'Must be put or call option'
     np.random.seed(seed)
     dt = T / d
     disc = np.exp(-r * dt)
@@ -18,9 +19,13 @@ def amput_lsm(S0, K, T, r, sigma, N, d, M, kind, av=False, cv=False, seed=42):
     S = np.exp((r - sigma**2 / 2) * dt + sigma * np.sqrt(dt) * W)
     S = S0 * np.vstack([np.ones(N), np.cumprod(S, axis=0)])
 
-    vals = np.maximum(K - S[-1, :], 0)
+    sign = 1
+    if kind == 'put':
+        sign = -1
+    
+    vals = np.maximum(sign * (S[-1, :] - K), 0)
     for t in range(d-1, 0, -1):
-        intrinsic = np.maximum(K - S[t, :], 0)
+        intrinsic = np.maximum(sign * (S[t, :] - K), 0)
         itm = intrinsic > 0
         if np.any(itm):
             X = S[t, itm]
@@ -33,10 +38,9 @@ def amput_lsm(S0, K, T, r, sigma, N, d, M, kind, av=False, cv=False, seed=42):
 
     # EU put as control
     if cv:
-        eu_lsm = np.maximum(K - S[-1, :], 0) * np.exp(-r * T)
+        eu_lsm = np.maximum(sign * (S[-1, :] - K), 0) * np.exp(-r * T)
         eu_bsm = bsm(S0, K, T, r, sigma, 'put')
         beta = np.cov(vals, eu_lsm)[0, 1] / np.var(eu_lsm)
         vals = vals - beta * (eu_lsm - eu_bsm) 
         
-    sd = np.std(vals, ddof=1)/np.sqrt(N)
-    return np.mean(vals), sd, [np.mean(vals) - 1.96*sd, np.mean(vals) + 1.96*sd]
+    return np.mean(vals)
