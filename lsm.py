f7ed9dcfff37743ef9c98c2edc1eec766fa53a22
emilholst14 <emilholst14@proton.me>
2025-04-23 10:49:50 +0200

    Update lsm.py

--- lsm.py
+++ lsm.py
@@ -1,43 +1,42 @@
-import numpy as np
-from bsm import BSM
+# American option with LSM
 
-def amput_lsm(S0, K, T, r, sigma, N, d, M, av=False, cv=False):
+import numpy as np
+from bsm import bsm
+
+def amput_lsm(S0, K, T, r, sigma, N, d, M, kind, av=False, cv=False, seed=42):
+    np.random.seed(seed)
     dt = T / d
     disc = np.exp(-r * dt)
 
     # Antithetical variables
     if av:
-        W = np.random.normal(0, np.sqrt(dt), size=(d, N // 2))
+        W = np.random.standard_normal((d, N // 2))
         W = np.concatenate((W, -W), axis=1)
     else:
-        W = np.random.normal(0, np.sqrt(dt), size=(d, N))
+        W = np.random.standard_normal((d, N))
         
-    S = np.exp((r - sigma**2 / 2) * dt + sigma * W)
-    S = S0 * np.vstack([np.ones(N), S]).cumprod(axis=0)
+    S = np.exp((r - sigma**2 / 2) * dt + sigma * np.sqrt(dt) * W)
+    S = S0 * np.vstack([np.ones(N), np.cumprod(S, axis=0)])
 
-    payoff = np.maximum(K - S, 0)
-    vals = payoff[-1, :]
+    vals = np.maximum(K - S[-1, :], 0)
     for t in range(d-1, 0, -1):
-        itm = payoff[t, :] > 0
+        intrinsic = np.maximum(K - S[t, :], 0)
+        itm = intrinsic > 0
         if np.any(itm):
             X = S[t, itm]
             Y = vals[itm] * disc
             reg = np.polyfit(X, Y, M)
-            cont = np.polyval(reg, X)
+            cont = np.zeros(N)
+            cont[itm] = np.polyval(reg, X)
+            exercise = intrinsic > cont
+            vals = np.where(exercise, intrinsic, vals * disc) 
 
-            exercise = payoff[t, itm] > cont
-            vals[itm] = np.where(exercise, payoff[t, itm], Y)
-    
-    V = vals * disc 
-
-    # EU option as control variate
+    # EU put as control
     if cv:
-        eu_lsm = np.maximum(K - S[-1], 0) * np.exp(-r * T)
-        eu_bsm = BSM(S0, K, T, r, sigma, 'put').price()
-        beta = np.cov(V, eu_lsm)[0, 1] / np.var(eu_lsm)
-        V = V - beta * (eu_lsm - eu_bsm) 
-    sd = np.std(V, ddof=1)/np.sqrt(N)
-    return np.mean(V), sd, [np.mean(V) - 1.96*sd, np.mean(V) + 1.96*sd]
-
-# np.random.seed(1203)
-# print(amput_lsm(100,100,2,0.05,0.2,100000,50,3,av=True, cv=True))
+        eu_lsm = np.maximum(K - S[-1, :], 0) * np.exp(-r * T)
+        eu_bsm = bsm(S0, K, T, r, sigma, 'put')
+        beta = np.cov(vals, eu_lsm)[0, 1] / np.var(eu_lsm)
+        vals = vals - beta * (eu_lsm - eu_bsm) 
+        
+    sd = np.std(vals, ddof=1)/np.sqrt(N)
+    return np.mean(vals), sd, [np.mean(vals) - 1.96*sd, np.mean(vals) + 1.96*sd]
